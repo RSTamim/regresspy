663d66dc2c11d17f395010f10f995a93b356329f
Md. Tamim Hasan <59114274+RSTamim@users.noreply.github.com>
2021-09-02 10:00:27 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # regresspy
-Lab project
---
+++ regresspy_Tamim/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'regresspy',
+    version = '1.1',
+    packages = ['regresspy.Regression'],
+    package_dir = {'' : 'regresspy'},
+    url = 'http://example.com',
+    liscense = 'MIT',
+    author = "Amit Chowdhury",
+    author_email = 'amit35-2475@diu.edu.bd',
+    description = 'Regression using Gradient Descent from scratch'
+)
